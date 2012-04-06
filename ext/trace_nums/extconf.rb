77b9d9cd60eb0d277fdb1935eb1c10cc1a46a235
Gabriel Horner <gabriel.horner@gmail.com>
2012-04-06 12:15:35 -0400

    simpler error handling when building extension

--- ext/trace_nums/extconf.rb
+++ ext/trace_nums/extconf.rb
@@ -1,14 +1,8 @@
 require "mkmf"
 require "debugger/ruby_core_source"
 
-if RUBY_VERSION >= "1.8"
-  if RUBY_RELEASE_DATE < "2005-03-22"
-    STDERR.print("Ruby version is too old\n")
-    exit(1)
-  end
-else
-  STDERR.print("Ruby version is too old\n")
-  exit(1)
+if RUBY_VERSION < '1.9'
+  abort("Ruby version is too old")
 end
 
 hdrs = proc {
@@ -17,10 +11,5 @@ hdrs = proc {
 
 dir_config("ruby")
 if !Debugger::RubyCoreSource.create_makefile_with_core(hdrs, "trace_nums19")
-  STDERR.print("Makefile creation failed\n")
-  STDERR.print("*************************************************************\n\n")
-  STDERR.print("  NOTE: For Ruby 1.9 installation instructions, please see:\n\n")
-  STDERR.print("     http://wiki.github.com/mark-moseley/ruby-debug\n\n")
-  STDERR.print("*************************************************************\n\n")
-  exit(1)
+  abort("Makefile creation failed.")
 end
